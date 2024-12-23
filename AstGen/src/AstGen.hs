03dd7516dec0c9cdf34dced25c69b540cf6d7edf
NzSN <nzsn0101@gmail.com>
2024-12-23 14:08:31 +0800

    fixup

--- AstGen/src/AstGen.hs
+++ AstGen/src/AstGen.hs
@@ -5,19 +5,17 @@ module AstGen (code_gen) where
 
 import Debug.Trace (trace)
 
-import Data.Semigroup (Semigroup(sconcat))
-import Data.Monoid (Monoid(mempty,mconcat))
-
 import Data.Aeson (decode, Object, (.:), (.:?))
 import Data.Aeson.Types (parseMaybe)
 import Data.ByteString.Lazy.Internal (ByteString)
-import Data.Maybe
+import Data.Maybe (catMaybes, fromJust, isJust)
 
 type Rule     = Object
 type NodeType = String
 type IsNamed  = Bool
 
-newtype ASTLiteral = ASTLiteral { get_source :: String } deriving (Show, Eq)
+data ASTLiteral = ASTLiteral { get_source :: String } |
+                  ASTEmpty deriving (Show, Eq)
 instance Semigroup ASTLiteral where
   l <> r = ASTLiteral $ get_source l ++ get_source r
 instance Monoid ASTLiteral where
@@ -30,9 +28,7 @@ code_gen bs = generate (decode bs :: Maybe [Rule])
   where
     generate :: Maybe [Rule] -> Maybe ASTLiteral
     generate Nothing      = Just $ ASTLiteral ""
-    generate (Just rules) =
-      let a = foldl' (\s r -> s <> generate_rule r) (Just $ ASTLiteral "") rules
-      in trace (show a) a
+    generate (Just rules) = foldl' (\s r -> s <> generate_rule r) (Just $ ASTLiteral "") rules
 
     generate_rule :: Rule -> Maybe ASTLiteral
     generate_rule r =
@@ -41,17 +37,41 @@ code_gen bs = generate (decode bs :: Maybe [Rule])
                 anonymous <- obj .: "named"
                 subnodes <- obj .:? "children"
 
-                case (subnodes :: Maybe String) of
+                case ((trace ("Sub:" ++ show subnodes) subnodes) :: Maybe Rule) of
                   -- Leaf node
                   Nothing -> return $ generate_leaf t anonymous
                   -- Internal node
-                  Just _ -> return $ generate_node r
+                  Just subnodes_ -> return $ generate_node t subnodes_
       in case v of
         Nothing -> Nothing
         Just v'  -> Just $ v'
 
     generate_leaf :: NodeType -> IsNamed -> ASTLiteral
-    generate_leaf = undefined
+    generate_leaf nt True = ASTLiteral $ "data  " ++ nt ++ " a = { ann : a, text : Data.Text.Internal.Text }"
+    generate_leaf nt False = ASTLiteral $ "type Anonymous_" ++ anonymousIdent nt ++ " = " ++  "Token " ++ nt
 
-    generate_node :: Rule -> ASTLiteral
-    generate_node = undefined
+    generate_node :: NodeType -> Rule -> ASTLiteral
+    generate_node nt sr =
+      let v = flip parseMaybe sr $ \obj -> do
+            children_types <- obj .: "types"
+            return $ generate_children children_types
+      in case v of
+        Nothing              -> ASTEmpty
+        Just (ASTEmpty)      -> ASTEmpty
+        Just (ASTLiteral v') -> ASTLiteral $
+          "data " ++ nt ++ " = " ++ "{ ann: a, extraChildren:" ++ v' ++ " }"
+
+    anonymousIdent :: NodeType -> String
+    anonymousIdent nt
+      | nt == "{"  = "left_brace"
+      | nt == "}"  = "right_brace"
+      | nt == "|"  = "pipe"
+      | nt == "|=" = "pipe_equal"
+      | nt == "||" = "pipe_pipe"
+      | nt == "~"  = "Tilde"
+      | otherwise  = nt
+
+
+    generate_children :: [Rule] -> ASTLiteral
+    generate_children r = do
+      foldl' (\s r -> ASTLiteral "AA") (ASTLiteral "") r
--- AstGen/test/Main.hs
+++ AstGen/test/Main.hs
@@ -1,22 +1,39 @@
 module Main (main) where
 
+
+import Debug.Trace (trace)
 import Test.HUnit
 
 import Data.ByteString.Lazy.Char8 (pack)
 import AstGen (code_gen)
 import Data.Maybe (isNothing)
 
-test1 :: Test
-test1 = TestCase (assert $ not $ isNothing $ code_gen (pack source))
+namedLeaf :: Test
+namedLeaf = TestCase (assert $ not $ isNothing $ code_gen (pack source))
   where source =
           "[\n\
           \{ \"type\": \"access_mode\", \n\
           \   \"named\": true, \n\
           \   \"fields\": {}}  \n\
           \]"
+anonyLeaf :: Test
+anonyLeaf = TestCase (assert $ not $ isNothing $ code_gen (pack source))
+  where source =
+          "[\n\
+          \{ \"type\": \"access_mode\", \n\
+          \   \"named\": false, \n\
+          \   \"fields\": {}}  \n\
+          \]"
+internalNode :: Test
+internalNode =
+  let v = code_gen (pack source)
+  in TestCase (assert $ not $ isNothing $ (trace (show v) v))
+  where source = "[{ \"type\": \"A\", \"named\": true, \"children\": { \"types\": [ { \"type\": \"B\", \"named\": true } ] }}]"
 
 tests :: Test
-tests = TestList [TestLabel "test1" test1]
+tests = TestList [TestLabel "NamedLeaf" namedLeaf,
+                  TestLabel "AnonyLeaf" anonyLeaf,
+                  TestLabel "Internal Node" internalNode]
 
 main :: IO Counts
 main = runTestTT tests
